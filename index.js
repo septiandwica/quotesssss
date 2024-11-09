4d74b51f57760a5895ccf6f240dd2c7d2c07175a
septiandwica <septiandwica03@gmail.com>
2024-11-10 00:05:53 +0700

    update

--- index.js
+++ index.js
@@ -51,6 +51,4 @@ app.get('/api/quote/affirmation', (req, res) => {
   
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
